8bcc4fdf6f764d20cace8c716dcee626e59889b3
Fabio Fragnito <fragnitofabio@gmail.com>
2024-09-30 14:54:53 +0200

    scritto % di sconto

--- js/main.js
+++ js/main.js
@@ -17,11 +17,13 @@ console.log(km);
 
 // 3rd fase: elaborazione
 
-if (eta < 18) { prezzoBiglietto = (x * 20) / 100 }
+if (eta < 18) { (prezzoBiglietto  * 20) / 100 };
 console.log(eta);
 
 
-if (eta > 65) { prezzoBiglietto = (x * 40) / 100 }
+if (eta > 65) { (prezzoBiglietto * 40) / 100 };
+console.log(eta);
+
 
 
 
