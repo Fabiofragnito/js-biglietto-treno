e314a4b6b521ace4ec988e48267de10dbf35f028
Fabio Fragnito <fragnitofabio@gmail.com>
2024-09-30 14:40:19 +0200

    2nd fase

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 // prima fase: preparazione
 let eta;
 let km;
+
+
+// 2nd fase: raccolta dati
 const prezzoKm = 0.21;
+let prezzoBiglietto = (km * prezzoKm);
+
+eta = prompt("scrivi la tua eta'");
+console.log(eta);
+km = prompt("scrivi quanti km vuoi percorrere");
+console.log(km);
+
+
+
+
+// 3rd fase: elaborazione
+
+
+
 
 
