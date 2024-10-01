d9e95bbd9571a91a2f69783790e705d7d433dc78
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-01 03:58:23 +0200

    formattato codice

--- js/main.js
+++ js/main.js
@@ -15,36 +15,24 @@ km = Number(prompt("scrivi quanti km vuoi percorrere"));
 console.log(km);
 
 let prezzoBiglietto = km * prezzoKm;
-
-
 console.log(prezzoBiglietto);
-
-
-
-
-
-
 
 
 // 3rd fase: elaborazione
 
-
-
 // Sconto del 20% per minorenni
 
-if (eta < 18)
-
-    { prezzoBiglietto = prezzoBiglietto * 0.80;
+if (eta < 18) {
+    
+prezzoBiglietto = prezzoBiglietto * 0.80;
 }
 
 
 // Sconto del 40% per over 65
-else if (eta > 65) 
+else if (eta > 65) {
 
-    { prezzoBiglietto = prezzoBiglietto * 0.60;
-   
+prezzoBiglietto = prezzoBiglietto * 0.60;
 }
-
 
 alert(`Il prezzo del biglietto è: ${prezzoBiglietto}€`);
 
